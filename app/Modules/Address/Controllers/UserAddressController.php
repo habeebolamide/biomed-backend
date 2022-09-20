523a96a5f99c5676ba78364cedf2cf6fb2feb61f
kodaoluidris <kodaoluidris@gmail.com>
2022-09-20 21:45:16 +0100

    FIXED ADDRESS

--- app/Modules/Address/Controllers/UserAddressController.php
+++ app/Modules/Address/Controllers/UserAddressController.php
@@ -3,6 +3,8 @@
 namespace App\Modules\Address\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Address\Requests\UserAddressRequest;
+use App\Modules\Address\Services\UserAddressServices;
 use Illuminate\Http\Request;
 
 class UserAddressController extends Controller
@@ -10,25 +12,35 @@ class UserAddressController extends Controller
     public function getUserAddress()
     {
     //    this will return user and address information
+        return (new UserAddressServices)->getUserAddresses();
     }
 
     public function getSingleUserAddress($user_address_id)
     {
         //    this will return single user and address information
+        return (new UserAddressServices)->getSingleAddress($user_address_id);
     }
 
-    public function updateUserAddress(Request $request, $user_address_id)
+    public function updateUserAddress(UserAddressRequest $request, $user_address_id)
     {
         // this will update single user and address information
+        return (new UserAddressServices)->updateSingleAddress($request->validated(), $user_address_id);
     }
 
     public function removeUserAddress($user_address_id)
     {
         # this will remove single address information of a user
+        return (new UserAddressServices)->removeSingleAddress($user_address_id);
     }
 
-    public function addUserAddress()
+    public function addUserAddress(UserAddressRequest $request)
     {
         # this will add single address information of a user
+        return (new UserAddressServices)->addSingleAddress();
+    }
+
+    public function defaultAddress($user_address_id)
+    {
+        return (new UserAddressServices)->defaultAddress($user_address_id);
     }
 }
