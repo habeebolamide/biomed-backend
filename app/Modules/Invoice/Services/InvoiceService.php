63e521ece935ea308f59dd536717f8b3d30e949c
Demscript <demscript@gmail.com>
2022-10-12 15:38:47 +0100

    fix

--- app/Modules/Invoice/Services/InvoiceService.php
+++ app/Modules/Invoice/Services/InvoiceService.php
@@ -18,7 +18,7 @@ class InvoiceService
     public function generateInvoice($data)
     {
         $validateUser=User::where('id', Auth::user()->id)->firstOrFail();
-
+        dd($validateUser);
         $userCart= Cart::with('product')->where('user_id', $validateUser->id)->get();
         if(count($userCart) < 1) return $this->badRequest('Cart empty');
         
