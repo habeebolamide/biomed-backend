5b5789e4da53b92749875f030a803d319d49720a
Demscript <demscript@gmail.com>
2022-10-14 11:34:09 +0100

    fix

--- app/Modules/Invoice/Models/Invoice.php
+++ app/Modules/Invoice/Models/Invoice.php
@@ -13,7 +13,7 @@ class Invoice extends Model
 {
     use HasFactory;
     protected $guarded = [];
-    protected $with = ["product","user","address","coupon"];
+    protected $with = ["product","user","address"];
     /**
      * The attributes that should be cast.
      */
