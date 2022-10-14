aaa669ade062aba5d4b9e8641866860620d43ef6
Demscript <demscript@gmail.com>
2022-10-14 12:30:13 +0100

    fix

--- app/Modules/Invoice/Models/Invoice.php
+++ app/Modules/Invoice/Models/Invoice.php
@@ -47,7 +47,7 @@ class Invoice extends Model
     public function createdAt() : Attribute
     {
         return new Attribute(
-            get: fn ($value) => Carbon::parse($value)->format('Y-M-d')
+            get: fn ($value) => Carbon::parse($value)->format('D M, Y')
         );
     }
 }
