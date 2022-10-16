f390f31f2fb8a935fbf66c926135148c43a2c8e3
Demscript <demscript@gmail.com>
2022-10-16 21:08:01 +0100

    fix

--- app/Modules/Transaction/Services/TransactionServices.php
+++ app/Modules/Transaction/Services/TransactionServices.php
@@ -3,10 +3,12 @@
 namespace App\Modules\Transaction\Services;
 
 use App\Modules\Invoice\Models\Invoice;
+use App\Modules\Transaction\Models\Transaction;
+use App\Traits\ApiResponseMessagesTrait;
 use Illuminate\Http\Request;
-
 class TransactionServices
 {
+    use ApiResponseMessagesTrait;
     public function initializeTransaction($data, $invoice_id)
     {
         // get the selected gateway_type
@@ -16,6 +18,17 @@ class TransactionServices
         $invoiceAmount = Invoice::where('invoice_id', $invoice_id)->sum('product_price');
         $invoice = Invoice::where('invoice_id', $invoice_id)->first();
         $totalAmount = $invoiceAmount - $invoice->coupon_disount/100 * $invoiceAmount;
-        return $totalAmount;
+        $reference_number = uniqid('TRANS');
+        
+        // Insert all datas into transaction table
+        $transaction = new Transaction;
+        $transaction->invoice_id = $invoice_id;
+        $transaction->user_id = $invoice->user_id;
+        $transaction->expected_amount = $totalAmount;
+        $transaction->reference_number = $reference_number;
+        $transaction->gateway_type = $gateway_type;
+        $transaction->save();
+        return $this->success($transaction, "Transaction Record Created");
+
     }
 }
