72e84ad8c6d8038b0a92530748797352cf1b54d0
Demscript <demscript@gmail.com>
2022-10-20 15:47:00 +0100

    fix

--- routes/api.php
+++ routes/api.php
@@ -39,6 +39,11 @@ Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
 Route::get('/auth/user_type', [AuthController::class, 'user_type'])->middleware('auth:sanctum');
 Route::get('/auth/logout', [AuthController::class, 'logout']);
+Route::get('/user-details', function()
+{
+    return auth()->user();
+});
+
 
 // ADMIN ROUTES
 Route::prefix('category')->middleware('auth:sanctum')->group(function(){
