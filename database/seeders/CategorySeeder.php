6901bd13b5006055955b04aba6a92477866a9443
kodaoluidris <kodaoluidris@gmail.com>
2022-10-24 10:31:06 +0100

    fixed some stuff

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -21,9 +21,11 @@ class CategorySeeder extends Seeder
     private function seedCategories()
     {
         return $cat = array(
-            array('category_name' => 'Veterinary Diagnostics', 'slug' => 'VTD', 'status' => 'active'),
-            array('category_name' => 'Human Diagnostic', 'slug' => 'HMD', 'status' => 'active'),
-            array('category_name' => 'Cell Culture, Cytology & Histology', 'slug' => 'CCH', 'status' => 'active'),
+            array('category_name' => 'Veterinary Diagnostics', 'slug' => 'VTD', 'status' => 'active', 'description' => "BioMed’s Veterinary diagnostics section offers a wide range of diagnostics kits such like ELISA, PCR, rapid test kits and any other technics or technologies that can  help in the diagnostics processes of the animal diseases and/or related disease"),
+            array('category_name' => 'Human Diagnostic', 'slug' => 'HMD', 'status' => 'active', 'description' => "BioMed’s Human diagnostics section offers a wide range of diagnostics kits and reagents such like ELISA, PCR, rapid test kits and any other technics our technologies that can help in the diagnostic process of the human diseases and/or related diseases"),
+            array('category_name' => 'Cell Culture, Cytology & Histology', 'slug' => 'CCH', 'status' => 'active', 'description' => "In this section you will find our wide range of Liquid and Powder Media provides the nutrients for all your cell culture applications. Each of these high-quality Cell Culture Media is manufactured according to the originally published formula, or is further developed and designed to meet highest expectations.
+            <br />
+            Apart from our Classic Media, such as DMEM or RPMI, we offer various ready-to-use Special Cell Culture Media and Cryopreservation Media. Our Special Media are specifically tailored to an application or cell type."),
         );
     }
 }
