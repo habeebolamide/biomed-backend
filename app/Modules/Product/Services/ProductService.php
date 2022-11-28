5da40a3d4e09f6be1568baba48ce5e4ccae2f1b4
Demscript <demscript@gmail.com>
2022-11-28 18:16:01 +0100

    fix

--- app/Modules/Product/Services/ProductService.php
+++ app/Modules/Product/Services/ProductService.php
@@ -113,9 +113,10 @@ class ProductService
           $product = DB::table('products')->join('product_diseases', 'product_diseases.id', 'products.product_disease_id')
                ->join('nested_sub_categories', 'nested_sub_categories.id', 'products.nested_sub_category_id')
                ->join('picture_products', 'picture_products.product_id', 'products.id')
+               ->join('pictures', 'pictures.id', 'picture_products.picture_id')
                ->join('sub_categories', 'sub_categories.id', 'nested_sub_categories.sub_category_id')
                ->join('categories', 'categories.id', 'sub_categories.category_id')
-               ->select('products.*', 'products.id','picture_products as picture');
+               ->select('*', 'products.id');
           if (!is_null($category_id)) {
                $product->where('category_id', $category_id);
           }
